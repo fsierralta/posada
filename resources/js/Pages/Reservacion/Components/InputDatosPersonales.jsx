11e0f600f729712b6d3a77290074412539c27303
fsierralta <sierralta.freddy@gmail.com>
2025-04-25 06:01:11 -0400

    Refactor: Update personal data input components
    - Updated `InputDatosPersonales.jsx` to improve data handling and validation.
    - Enhanced `InputPersona.jsx` for better user experience and maintainability.

--- resources/js/Pages/Reservacion/Components/InputDatosPersonales.jsx
+++ resources/js/Pages/Reservacion/Components/InputDatosPersonales.jsx
@@ -4,163 +4,139 @@ import PrimaryButton from "@/Components/PrimaryButton"
 
 export default function InputDatosPersonales({data,setData,errors,onGetHupespede}) {
   return (
-     <div className="mt-8">
-        <hr />
-        <div className="flex gap-2 mt-2">
-                                <div>
-                                          <InputLabel>Nacionalidad
-                                            <select
-                                              name="nacionalidad"
-                                              value={data.nacionalidad}
-                                              onChange={e => setData('nacionalidad', e.target.value)}
-                                              className="rounded-md w-full"
-                                            >
-                                              <option value="V">Venezolano</option>
-                                              <option value="E">Extranjero</option>
-                                            </select>
-                                          </InputLabel>
-                                          {errors.nacionalidad && <div className="text-red-500">{errors.nacionalidad}</div>}
-                                </div>
-                                <div className=''>
-                                    <InputLabel>{`Cédula`}
-                                      <TextInput
-                                        type='text'
-                                        name='cedula'
-                                        value={data.cedula}
-                                        placeholder="nro"
-                                        onChange={e => setData('cedula', e.target.value)}
-                                        className='w-full'
-                                        required
-    
-                                      />
-                                    </InputLabel>
-                                    {errors.cedula && <div className="text-red-500">{errors.cedula}</div>}
-                                </div>
-                                <div>
-                                  <InputLabel>
-                                    Realizar
-                                    </InputLabel>
-                                  <PrimaryButton className=''
-                                     onClick={onGetHupespede}
-                                   >Buscar</PrimaryButton>
-                                   
-                                  
-                                </div>
-
-                                <div>
-                                  <InputLabel>
-                                      Nombre
-    
-                                  </InputLabel>
-                                  <TextInput
-                                     name='nombre'
-                                     value={data.nombre}
-                                     onChange={e=>setData('nombre',e.target.value) }
-                                     required
-                                  />
-                                </div>
-
-                                <div className=''>
-                                <InputLabel>
-                                  Apellidos
-                                </InputLabel>
-                                <TextInput
-                                  name='apellidos'
-                                  type="text"
-                                  value={data.apellidos}
-                                    onChange={e => setData('apellidos', e.target.value)}
-                                    required
-                                    
-                                />
-                                </div>
-                             
-                                <div>
-                                      <InputLabel>
-                                        Fecha de Nacimiento
-                                      </InputLabel>
-                                      <TextInput
-                                        type='date'
-                                        name='nacimiento'
-                                        value={data.nacimiento}
-                                        onChange={e => setData('nacimiento', e.target.value)}
-                                        className='w-full'
-                                        required
-                                        
-                                      />
-                                      {errors?.nacimiento &&<div className='text-red-500'>{errors.nacimiento}</div>}
-    
-                                </div>
-    
-                           
-                           
-        </div> 
-        <div className=" flex gap-2 flex-row">                     
-                 <div className='flex gap-2 mt-4'>
-                                  
-                                    <div >
-                                      <InputLabel>
-                                      Email
-                                      </InputLabel>
-                                      <TextInput 
-                                        type="email"
-                                        name="email"
-                                        value={data.email}
-                                        onChange={e => setData('email', e.target.value)}
-                                        className=""
-                                        required
-                                      />
-                                      
-                                      {errors.email && <div className="text-red-500">{errors.email}</div>}
-                                    </div>
-                                  
-                                  <div>
-                                    <InputLabel>
-                                      Celular
-                                    </InputLabel>  
-                                      <TextInput
-                                        type="text"
-                                        name="celular"
-                                        value={data.celular}
-                                        onChange={e => setData('celular', e.target.value)}
-                                        className=""
-                                        required
-                                      />
-                                    
-                                  </div>
-                                  <div>
-                                    <InputLabel>
-                                      Procedencia
-                                    </InputLabel>
-                                    <TextInput
-                                      type="text"
-                                      name="procedencia"
-                                      value={data.procedencia}
-                                      onChange={e => setData('procedencia', e.target.value)}
-                                      className=""
-                                      required
-                                    />
-    
-                                  </div>
-                                  <div>
-                                    <InputLabel>
-                                      Profesión
-                                    </InputLabel>
-                                    <TextInput
-                                      type="text"
-                                      name="profesion"
-                                      value={data.profesion}
-                                      onChange={e => setData('profesion', e.target.value)}
-                                      className=""
-                                      required
-                                    />
-                                  </div>
-    
-    
-                 </div>
-         </div>
-     </div>
-
-
-  
+    <div className="mt-8">
+      <hr />
+      {/* Agrupa los campos principales en un grid responsivo */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-2">
+        <div>
+          <InputLabel>Nacionalidad
+            <select
+              name="nacionalidad"
+              value={data.nacionalidad}
+              onChange={e => setData('nacionalidad', e.target.value)}
+              className="rounded-md w-full"
+            >
+              <option value="V">Venezolano</option>
+              <option value="E">Extranjero</option>
+            </select>
+          </InputLabel>
+          {errors.nacionalidad && <div className="text-red-500">{errors.nacionalidad}</div>}
+        </div>
+        <div>
+          <InputLabel>{`Cédula`}
+            <TextInput
+              type='text'
+              name='cedula'
+              value={data.cedula}
+              placeholder="nro"
+              onChange={e => setData('cedula', e.target.value)}
+              className='w-full'
+              required
+            />
+          </InputLabel>
+          {errors.cedula && <div className="text-red-500">{errors.cedula}</div>}
+        </div>
+        <div className="flex items-end">
+          <PrimaryButton className='w-full' onClick={onGetHupespede}>
+            Buscar
+          </PrimaryButton>
+        </div>
+        <div>
+          <InputLabel>
+            Nombre
+          </InputLabel>
+          <TextInput
+            name='nombre'
+            value={data.nombre}
+            onChange={e=>setData('nombre',e.target.value) }
+            required
+            className="w-full"
+          />
+        </div>
+        <div>
+          <InputLabel>
+            Apellidos
+          </InputLabel>
+          <TextInput
+            name='apellidos'
+            type="text"
+            value={data.apellidos}
+            onChange={e => setData('apellidos', e.target.value)}
+            required
+            className="w-full"
+          />
+        </div>
+        <div>
+          <InputLabel>
+            Fecha de Nacimiento
+          </InputLabel>
+          <TextInput
+            type='date'
+            name='nacimiento'
+            value={data.nacimiento}
+            onChange={e => setData('nacimiento', e.target.value)}
+            className='w-full'
+            required
+          />
+          {errors?.nacimiento &&<div className='text-red-500'>{errors.nacimiento}</div>}
+        </div>
+      </div>
+      {/* Agrupa los campos secundarios en otro grid responsivo */}
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
+        <div>
+          <InputLabel>
+            Email
+          </InputLabel>
+          <TextInput 
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={e => setData('email', e.target.value)}
+            className="w-full"
+            required
+          />
+          {errors.email && <div className="text-red-500">{errors.email}</div>}
+        </div>
+        <div>
+          <InputLabel>
+            Celular
+          </InputLabel>  
+          <TextInput
+            type="text"
+            name="celular"
+            value={data.celular}
+            onChange={e => setData('celular', e.target.value)}
+            className="w-full"
+            required
+          />
+        </div>
+        <div>
+          <InputLabel>
+            Procedencia
+          </InputLabel>
+          <TextInput
+            type="text"
+            name="procedencia"
+            value={data.procedencia}
+            onChange={e => setData('procedencia', e.target.value)}
+            className="w-full"
+            required
+          />
+        </div>
+        <div>
+          <InputLabel>
+            Profesión
+          </InputLabel>
+          <TextInput
+            type="text"
+            name="profesion"
+            value={data.profesion}
+            onChange={e => setData('profesion', e.target.value)}
+            className="w-full"
+            required
+          />
+        </div>
+      </div>
+    </div>
   )
 }
--- resources/js/Pages/Reservacion/Components/InputPersona.jsx
+++ resources/js/Pages/Reservacion/Components/InputPersona.jsx
@@ -4,106 +4,86 @@ import PrimaryButton from "@/Components/PrimaryButton"
 
 export default function InputPersona({data,setData,errors,precioValor,formaPagos}) {
   return (
-     <div className='flex gap-2 mt-2 ' >
-                                <div className=''>
-                                  <InputLabel>Nro Personas
-                                  <TextInput
-                                    type='number'
-                                    name='nro_personas'
-                                    value={data.nro_personas}
-                                    onChange={e => setData('nro_personas', e.target.value)}
-                                    required
-                                    min="1"
-                                    max="100"
-                                    className=''
-                                  />
-                                  </InputLabel>
-                                 
-                                  {errors.nro_personas && <div className="text-red-500">{errors.nro_personas}</div>}
-                                </div>
-                                <div className='w-full'>
-                
-                                  <InputLabel>Cant. C
-                                  <TextInput
-                                    type='number'
-                                    name='cantidad_cabana_reservadas'
-                                    value={data.cantidad_cabana_reservadas}
-                                    onChange={e => setData('cantidad_cabana_reservadas', e.target.value)}
-                                    required
-                                    min="1"
-                                    max="9"
-                                    className='w-full'
-                                  />
-                                  </InputLabel>
-                                 
-                                  {errors.cantidad_cabana_reservadas && <div className="text-red-500">{errors.cantidad_cabana_reservadas}</div>}
-                                </div>
-                
-                                <div className=''>
-                                  <InputLabel>Total a Pagar
-                                  <TextInput
-                                    type='number'
-                                    name='totalPagar'
-                                    step="0.01"
-                                    value={data.totalPagar}
-                                    className=''
-                                   //onChange={e => setData('totalPagar', e.target.value)}
-                                    disabled
-                                   
-                                  />
-                                  </InputLabel>
-                                  {errors.totalPagar && <div className="text-red-500">{errors.totalPagar}</div>}
-                                 
-                
-                                </div>
-                                
-                                <div className=''>
-                                    <InputLabel>Calcular Pago</InputLabel> 
-                                        <PrimaryButton className='py-3 w-full'
-                                           onClick={precioValor}
-                                        >Calcular</PrimaryButton>
-                                 </div>
-                                 <div className='w-full'>
-                                    <InputLabel>Forma de Pago
-                                    <select
-                                      name="pago_id"
-                                      value={data.pago_id}
-                                      onChange={e => setData('pago_id', e.target.value)}
-                                      className="rounded-md "
-    
-                                    >
-                                      <option value="">Forma de pago</option>
-                                      {formaPagos.map(formaPago => (
-                                        <option key={formaPago.id} value={formaPago.id}>
-                                          {formaPago.nombre}
-                                        </option>
-                                      ))}
-                                    </select>
-                                    </InputLabel>
-                                   
-                                    {errors.pago_id && <div className="text-red-500">{errors.pago_id}</div>}
-                
-                                 </div>
-                                 <div >
-                                   
-                                  <div className=''>
-                                    <InputLabel>Observación:
-                                    <TextInput
-                                      type='text'
-                                      name='observacion'
-                                      value={data.observacion}
-                                      onChange={e => setData('observacion', e.target.value)}
-                                      className=''
-                                      required
-                                    />
-                                    </InputLabel>
-                                   
-                                    {errors.observacion && <div className="text-red-500">{errors.observacion}</div>}
-                                  </div>
-                                 </div>
-    
-                                
-                                  </div>
-                          
+    <div className="flex flex-col md:flex-row md:flex-wrap gap-4 mt-2 w-full">
+      <div className="flex-1 min-w-[180px]">
+        <InputLabel>N.Personas
+          <TextInput
+            type='number'
+            name='nro_personas'
+            value={data.nro_personas}
+            onChange={e => setData('nro_personas', e.target.value)}
+            required
+            min="1"
+            max="100"
+            className='w-full'
+          />
+        </InputLabel>
+        {errors.nro_personas && <div className="text-red-500">{errors.nro_personas}</div>}
+      </div>
+      <div className="flex-1 min-w-[180px]">
+        <InputLabel>Cant. Cabañas
+          <TextInput
+            type='number'
+            name='cantidad_cabana_reservadas'
+            value={data.cantidad_cabana_reservadas}
+            onChange={e => setData('cantidad_cabana_reservadas', e.target.value)}
+            required
+            min="1"
+            max="9"
+            className='w-full'
+          />
+        </InputLabel>
+        {errors.cantidad_cabana_reservadas && <div className="text-red-500">{errors.cantidad_cabana_reservadas}</div>}
+      </div>
+      <div className="flex-1 min-w-[180px]">
+        <InputLabel>Total a Pagar
+          <TextInput
+            type='number'
+            name='totalPagar'
+            step="0.01"
+            value={data.totalPagar}
+            className='w-full'
+            disabled
+          />
+        </InputLabel>
+        {errors.totalPagar && <div className="text-red-500">{errors.totalPagar}</div>}
+      </div>
+      <div className="flex-1 min-w-[180px] flex items-end">
+        <PrimaryButton className='py-3 w-full' onClick={precioValor}>
+          Calcular
+        </PrimaryButton>
+      </div>
+      <div className="flex-1 min-w-[180px]">
+        <InputLabel>Forma de Pago
+          <select
+            name="pago_id"
+            value={data.pago_id}
+            onChange={e => setData('pago_id', e.target.value)}
+            className="rounded-md w-full"
+          >
+            <option value="">Forma de pago</option>
+            {formaPagos.map(formaPago => (
+              <option key={formaPago.id} value={formaPago.id}>
+                {formaPago.nombre}
+              </option>
+            ))}
+          </select>
+        </InputLabel>
+        {errors.pago_id && <div className="text-red-500">{errors.pago_id}</div>}
+      </div>
+      <div className="flex-1 min-w-[180px]">
+        <InputLabel>Observación:
+          <TextInput
+            type='text'
+            name='observacion'
+            value={data.observacion}
+            onChange={e => setData('observacion', e.target.value)}
+            className='w-full'
+            required
+          />
+        </InputLabel>
+        {errors.observacion && <div className="text-red-500">{errors.observacion}</div>}
+      </div>
+    </div>
   )
 }
